4cb550ce80630fc77733528bd81569f451115742
aronet59 <47854965+aronet59@users.noreply.github.com>
2020-07-17 08:39:49 -0400

    Add files via upload

---
+++ holdforrelease/SQL Server Jobs_16/DBA  Update Expiration History.sql
@@ -0,0 +1,73 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [DBA  Update Expiration History]    Script Date: 6/17/2019 10:00:02 AM ******/
+EXEC msdb.dbo.sp_delete_job @job_id=N'af036b99-13fe-4d7a-bba5-91e854fdec9a', @delete_unused_schedule=1
+GO
+
+/****** Object:  Job [DBA  Update Expiration History]    Script Date: 6/17/2019 10:00:02 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 6/17/2019 10:00:02 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA  Update Expiration History', 
+		@enabled=0, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'update the restore date in the database expiration table and thus the history table.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'dbservices', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Update expiration history]    Script Date: 6/17/2019 10:00:02 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Update expiration history', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC p_dba16_dba_UpdateDatabaseExpirationRestore', 
+		@database_name=N'dba_db16', 
+		@flags=4
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'dba update expiration history', 
+		@enabled=1, 
+		@freq_type=8, 
+		@freq_interval=127, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=4, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20100224, 
+		@active_end_date=99991231, 
+		@active_start_time=33000, 
+		@active_end_time=5959, 
+		@schedule_uid=N'1f748c87-39e1-467d-9cd1-e268f27d13cb'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
